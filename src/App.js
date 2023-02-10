b1d708b6647278c5232c9c4537e0279fb535ca13
Harris <hbkaries@gmail.com>
2023-02-10 12:54:14 +0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
-function App() {
+const Calculator = () => {
+  const [value, setValue] = useState('');
+  const [prevValue, setPrevValue] = useState(null);
+  const [operator, setOperator] = useState(null);
+
+  const handleClick = (e) => {
+    const btnValue = e.target.innerHTML;
+    if (btnValue === 'C') {
+      setValue('');
+      setPrevValue(null);
+      setOperator(null);
+    } else if (btnValue === '=') {
+      if (operator === '+') {
+        setValue(prevValue + parseFloat(value));
+      } else if (operator === '-') {
+        setValue(prevValue - parseFloat(value));
+      } else if (operator === 'x') {
+        setValue(prevValue * parseFloat(value));
+      } else if (operator === '÷') {
+        if (value === '0') {
+          alert("Can't divide by 0");
+          setValue('');
+          setPrevValue(null);
+          setOperator(null);
+        } else {
+          setValue(prevValue / parseFloat(value));
+        }
+      }
+    } else if (isNaN(btnValue)) {
+      setPrevValue(parseFloat(value));
+      setValue('');
+      setOperator(btnValue);
+    } else {
+      setValue(value + btnValue);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="calculator">
+      <input type="text" value={value} />
+      <br />
+      <br />
+      <button onClick={handleClick}>1</button>
+      <button onClick={handleClick}>2</button>
+      <button onClick={handleClick}>3</button>
+      <button onClick={handleClick}>+</button>
+      <br />
+      <button onClick={handleClick}>4</button>
+      <button onClick={handleClick}>5</button>
+      <button onClick={handleClick}>6</button>
+      <button onClick={handleClick}>-</button>
+      <br />
+      <button onClick={handleClick}>7</button>
+      <button onClick={handleClick}>8</button>
+      <button onClick={handleClick}>9</button>
+      <button onClick={handleClick}>x</button>
+      <br />
+      <button onClick={handleClick}>C</button>
+      <button onClick={handleClick}>0</button>
+      <button onClick={handleClick}>=</button>
+      <button onClick={handleClick}>÷</button>
     </div>
   );
-}
+};
 
-export default App;
+export default Calculator;
+
